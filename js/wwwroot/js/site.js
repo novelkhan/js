2ddc8bdec4bd3cff21e726c98bc822bb2516484e
novelkhan <novel4004@gmail.com>
2022-12-26 02:51:38 +0600

    Add project files.

---
+++ js/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//var on = "/images/pic_bulbon.gif";
+//var off = "/images/pic_bulboff.gif";
+
+
+//function myFunction() {
+//    document.getElementById("demo").innerHTML = "Paragraph changed.";
+//}
+
+//function backFunction() {
+//    document.getElementById("demo").innerHTML = "A Paragraph.";
+//}
+
+document.getElementById('indexTime').innerHTML = Date();
