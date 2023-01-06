de9aeb8ea7f0161a1d5a19d3705acd19eba85f2f
novelkhan <novel4004@gmail.com>
2023-01-07 05:04:13 +0600

    Man API is called using js

--- js/wwwroot/js/site.js
+++ js/wwwroot/js/site.js
@@ -15,4 +15,154 @@
 //    document.getElementById("demo").innerHTML = "A Paragraph.";
 //}
 
-document.getElementById('indexTime').innerHTML = Date();
+//document.getElementById('indexTime').innerHTML = Date();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.clear();
+
+// event - onload(), onerror()
+// property - response, responseText, responseType, responseURL, status, statusText
+// function - open(), send(), setRequestHeader()
+
+
+const makeRequest = (method, url, data) => {
+
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+
+        xhr.setRequestHeader('Content-Type', 'application/json');
+
+        xhr.onload = () => {
+            let data = xhr.response;
+            console.log(JSON.parse(data))
+        }
+
+        xhr.onerror = () => {
+            console.log('error is here');
+        }
+
+        xhr.send(JSON.stringify(data));
+    });
+}
+
+
+// const getData = () => {
+// makeRequest('GET', 'https://img-api.azurewebsites.net/api/Person')
+// .then((res) => console.log(res))
+// }
+// getData();
+
+
+
+const getData = () => {
+    makeRequest('GET', 'https://img-api.azurewebsites.net/api/Man')
+        .then((res) => console.log(res))
+}
+getData();
+
+
+// const sendData = () => {
+    // makeRequest('POST', 'https://img-api.azurewebsites.net/api/Man', {
+        // Name: 'Bossco Khan',
+        // Age: 29,
+        // City: 'Tangail',
+        // Country: 'Bangladesh',
+    // });
+// }
+// sendData();
+
+
+ // const updateData = () => {
+     // makeRequest('PATCH', 'https://img-api.azurewebsites.net/api/Man/3', {
+         // Name: 'Rukan Khan Again',
+         // Age: 29,
+         // City: 'Tangail',
+         // Country: 'Bangladesh',
+     // });
+// }
+// updateData();
+
+// const updateSingleData = () => {
+    // makeRequest('PATCH', 'https://img-api.azurewebsites.net/api/Man/3', {
+
+        // Name: 'This is changed',
+
+    // });
+// }
+// updateSingleData();
+
+
+
+// const deleteData = () => {
+    // makeRequest('DELETE', 'https://img-api.azurewebsites.net/api/Man/3');
+// }
+// deleteData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const getData = () => {
+
+	// const xhr = new XMLHttpRequest();
+	// xhr.open('GET', 'https://img-api.azurewebsites.net/api/Man');
+	// xhr.onload = () => {
+		// let data = xhr.response;
+		// console.log(data);
+	// }
+	// xhr.send();
+
+// }
+
+// getData();
